663c7f0614a4e917c1e2c8bc2557889f40e54cb9
Andrew Golightly <support@andrewgolightly.com>
2013-10-03 12:52:19 -0700

    finally got the senders tags parsing correctly to return the submitters email address

--- wpcf7-email-verification.php
+++ wpcf7-email-verification.php
@@ -33,54 +33,37 @@ function wpcf7ev_debug( $message ) {
     wp_mail( 'support@andrewgolightly.com', 'Debug code', print_r($message, true));
 }
 
-/* get the tags from the senders field in Contact Form 7 and return the actual
-post_data for those tags
+/*
+    get the tags from the senders field in Contact Form 7 and return the actual
+    post_data for those tags
+    
+    e.g. "[your-name] <[your-email]>" --> "Harry <harry@potter.com>"
 */
 function wpcf7ev_get_senders_email_address($wpcf7_form)
-{
-    wpcf7ev_debug("Getting email address to verify...");
-    $tagName = "your-name";
-    wpcf7ev_debug($wpcf7_form->posted_data[$tagName]);
-    
+{    
+    // grab sender info    
     $senderTags = $wpcf7_form->mail['sender'];
     
-    // replace <> with posted_data
-    
-    $sendersEmailAddress = preg_replace_callback('/\[(.+?)\]/',
-                                                function ($matches)
-                                                {
-                                                    //wpcf7ev_debug($wpcf7_form->posted_data);
-                                                    $currentTag = $wpcf7_form->posted_data[$tagName];
-                                                    wpcf7ev_debug($currentTag);
-                                                    //return 
-                                                    //wpcf7ev_debug($matches);
-                                                    return "bob";
-                                                },
-                                                $senderTags
-                                               );
-                                               
-    //$sendersEmailAddress = preg_replace('/\[(.+?)\]/', "bob", $senderTags);
-    
-    wpcf7ev_debug("Senders email address: " . $sendersEmailAddress);
+    // replace tag names with posted_data using regex
+    return $sendersEmailAddress = preg_replace_callback('/\[(.+?)\]/',
+                                 function ($matches) use ($wpcf7_form)
+                                 {
+                                     return $wpcf7_form->posted_data[$matches[1]];
+                                 },
+                                 $senderTags
+                                 );
 }
 
 function wpcf7ev_verify_email_address( &$wpcf7_form )
 {
     // get the email address it's being sent from
-    /* 
-        todo: I wonder if there is a way to not hardcode in "your-email" as senders email address?
-        Something to do with parsing $wpcf7_form->mail['sender']; ??
-    */
-    //wpcf7ev_debug( "Need to verify the following email address " .  $submittersEmailAddress);
     $submittersEmailAddress = wpcf7ev_get_senders_email_address($wpcf7_form);
+    wpcf7ev_debug("Need to verify " . $submittersEmailAddress);
     
     // save submitted form to the database
     
-    // get the email address it's being sent from
-    
-    
     // send email to the submitter with a verification link to click on
-    //$submittersEmail = $wpcf7_form->posted_data["your-email"];
+    wp_mail($submittersEmailAddress , 'Debug code', "Thanks for your message.");
     
     // prevent the form being sent as per usual
     $wpcf7_form->skip_mail = true;
