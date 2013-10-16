4839e4528f1576be37606c345b4ad4debc29cc84
Andrew Golightly <support@andrewgolightly.com>
2013-10-16 10:39:32 +0700

    Tidied up the sending of the form data using CF7 core code

--- wpcf7-email-verification.php
+++ wpcf7-email-verification.php
@@ -111,14 +111,13 @@ function check_for_verifier() {
             }
             else
             {
-                wpcf7ev_debug("We have a match!!");
-                $cf7 = $storedValue[0];
-                $cf7->compose_mail( $cf7->setup_mail_template( $cf7->mail, 'mail' ) );
-                    
-                if ( $cf7->mail_2['active'] )
-                    $cf7->compose_mail( $cf7->setup_mail_template( $cf7->mail_2, 'mail_2' ) );
-                
-                // Delete the transient to make sure the emails can't be resent if that verification link
+                // remove the action that triggers this plugin's code
+                remove_action( 'wpcf7_before_send_mail', 'wpcf7ev_verify_email_address' );
+                $cf7 = $storedValue[0]; // get the saved CF7 object
+                $cf7->skip_mail = false; // allow mail to be sent as per usual
+                $cf7->mail(); // send mail using the CF7 core code
+                        
+                // Delete the transient to make sure the emails can't be re-sent if that verification link
                 // is clicked on again.
                 delete_transient($slug);
             }
