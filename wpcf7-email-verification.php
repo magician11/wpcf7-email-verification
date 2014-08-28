e205ac250783b72781e46393dc2842dea588f2cb
Andrew Golightly <andrew@golightlyplus.com>
2014-08-28 10:44:10 +0200

    basic saving, retrieving and submitting form submissions now works again

--- wpcf7-email-verification.php
+++ wpcf7-email-verification.php
@@ -10,7 +10,7 @@
  * License: GPL2
  */
 
-/*  Copyright 2013  Andrew Golightly  (email : andrew@golightlyplus.com)
+/*  Copyright 2014  Andrew Golightly  (email : andrew@golightlyplus.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -36,25 +36,40 @@ define('WPCF7EV_STORAGE_TIME', 16 * HOUR_IN_SECONDS);
  * Intercept Contact Form 7 forms being sent by first verifying the senders email address.
  */
 
-// first prevent the emails being sent as per usual
-add_filter('wpcf7_mail_components', 'wpcf7ev_skip_sending');
-
 function wpcf7ev_skip_sending($components) {
-    
+
     $components['send'] = false;
-    
+
     return $components;
 }
+
+// prettify the email addresses being sent
+add_filter( 'wp_mail_from', function($email_address){
+
+    return get_option('admin_email');
+}, 9);
+
+add_filter( 'wp_mail_from_name', function($from_name){
+
+    return get_option('blogname');
+}, 9);
 
 // then request the email address to be verified and save the submission as a transient
 add_action( 'wpcf7_before_send_mail', 'wpcf7ev_verify_email_address' );
 
 function wpcf7ev_verify_email_address( $wpcf7_form )
 {
+
+    // first prevent the emails being sent as per usual
+    add_filter('wpcf7_mail_components', 'wpcf7ev_skip_sending');
+
     $mail_tags = $wpcf7_form->prop('mail');
     $mail_fields = wpcf7_mail_replace_tags( $mail_tags );
     $senders_email_address = $mail_fields['sender'];
-    
+
+    wp_mail('support@andrewgolightly.com', 'mail fields', print_r($mail_fields, true));
+    //wp_die("testing");
+
     // send an email to the recipient to let them know verification is pending
     wp_mail($mail_fields['recipient'], 'Form notice',
             "Hi,\n\nYou've had a form submission on " . get_option('blogname') . " from " .
@@ -65,18 +80,8 @@ function wpcf7ev_verify_email_address( $wpcf7_form )
     $random_hash = substr(md5(uniqid(rand(), true)), -16, 16);
 
     // save submitted form as a transient object
-    wpcf7ev_save_form_submission($wpcf7_form, $random_hash);
-
-    add_filter( 'wp_mail_from', function($email_address){
-
-        return get_option('admin_email');
-    });
-
-    add_filter( 'wp_mail_from_name', function($from_name){
-
-        return get_option('blogname');
-    });
-
+    $data_to_save = array($mail_fields, $random_hash);
+    set_transient( wpcf7ev_get_slug($random_hash), $data_to_save , WPCF7EV_STORAGE_TIME );
 
     // send email to the sender with a verification link to click on
     wp_mail($senders_email_address , 'Verify your email address',
@@ -85,11 +90,20 @@ function wpcf7ev_verify_email_address( $wpcf7_form )
             get_site_url() . "/wp-admin/admin-post.php?action=wpcf7ev&email-verification-key={$random_hash}" . "\n\nThanks.");
 }
 
+add_action('wpcf7_mail_sent', 'wpcf7ev_cleanup');
+add_action('wpcf7_mail_failed', 'wpcf7ev_cleanup');
+
+function wpcf7ev_cleanup() {
+    // remove the action that triggers this plugin's code
+    remove_action( 'wpcf7_before_send_mail', 'wpcf7ev_verify_email_address' );
+    remove_filter( 'wpcf7_mail_components', 'wpcf7ev_skip_sending' ); // allow mail to be sent as per usual
+}
+
 /**
  * Save the Contact Form 7 object as transient data.
  * The saved object is automatically serialized.
  */
-
+/*
 function wpcf7ev_save_form_submission($cf7_object, $random_hash) {
 
     // make a copy of the cf7 object so that it keeps a reference to the original filepaths
@@ -120,6 +134,7 @@ function wpcf7ev_save_form_submission($cf7_object, $random_hash) {
 
     set_transient( wpcf7ev_get_slug($random_hash), $data_to_save , WPCF7EV_STORAGE_TIME );
 }
+*/
 
 /**
  * Create the slug key for the transient CF7 object
@@ -142,6 +157,8 @@ add_action( 'admin_post_nopriv_wpcf7ev', 'wpcf7ev_check_verifier' );
 
 // check the verification key
 function wpcf7ev_check_verifier() {
+
+    set_current_screen('wpcf7ev');
 
     // output the header of the theme being used
     status_header(200);
@@ -167,16 +184,14 @@ function wpcf7ev_check_verifier() {
             }
             else
             {
-                // remove the action that triggers this plugin's code
-                remove_action( 'wpcf7_before_send_mail', 'wpcf7ev_verify_email_address' );
-                remove_filter( 'wpcf7_mail_components', 'wpcf7ev_skip_sending' ); // allow mail to be sent as per usual
-                $cf7 = $storedValue[0]; // get the saved CF7 object
-                wp_mail('support@andrewgolightly.com', 'saved object', print_r($cf7, true));
-                WPCF7_Mail::send( $cf7->prop( 'mail' ), 'mail' ); // send mail using the CF7 core code
+                $cf7_mail_fields = $storedValue[0]; // get the saved CF7 object
+                wp_mail('support@andrewgolightly.com', 'retrieved object', print_r($cf7_mail_fields, true));
+                wp_mail($cf7_mail_fields['recipient'], $cf7_mail_fields['subject'], $cf7_mail_fields['body']);
+
                 // display a confirmation message then redirect back to the homepage after 8 seconds
-                echo('<h2>Thank you. Verification key accepted.</h2>' . 
-                     '<p>Your form submission will now be processed.</p>' . 
-                     '<p>If you are not redirected back to the homepage in 8 seconds, <a href="' . get_site_url() . '">click here</a>.</p>' .
+                echo('<h2 style="text-align:center;">Thank you. Verification key accepted.</h2>' . 
+                     '<p style="text-align:center;">Your form submission will now be processed.</p>' . 
+                     '<p style="text-align:center;">If you are not redirected back to the homepage in 8 seconds, <a href="' . get_site_url() . '">click here</a>.</p>' .
                      '<script> setTimeout(function () { window.location.href = "' . get_site_url() . '"; }, 8000); </script>');
             }
         }
@@ -190,6 +205,7 @@ function wpcf7ev_check_verifier() {
  */
 
 // this hook gets called everytime a form submission is made (verified or not)
+/*
 add_action( 'wpcf7_mail_sent', 'wpcf7ev_cleanup_attachments' );
 
 function wpcf7ev_cleanup_attachments() {
@@ -211,5 +227,5 @@ function wpcf7ev_cleanup_attachments() {
         closedir( $handle );
     }
 }
-
+*/
 ?>
